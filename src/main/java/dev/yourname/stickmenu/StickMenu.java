98a251972e6ebd8490f134577137f01377908582
trevorreedy <trevinator001@gmail.com>
2025-06-24 11:55:16 -0500

    update

--- src/main/java/dev/yourname/stickmenu/StickMenu.java
+++ src/main/java/dev/yourname/stickmenu/StickMenu.java
@@ -43,11 +43,14 @@ public class StickMenu extends JavaPlugin implements Listener {
                 .content("Choose an option:")
                 .button("Teleport Requests")
                 .button("Homes")
+                .button("Add Home")
+                .button("Warps")
                 .validResultHandler(response -> {
                     int buttonId = response.clickedButtonId();
                         switch (buttonId) {
                             case 0 -> player.performCommand("tpa");
                             case 1 -> player.performCommand("homes");
+                            case 2 -> player.performCommand("sethome");
                         }
                 })
                 .build();
